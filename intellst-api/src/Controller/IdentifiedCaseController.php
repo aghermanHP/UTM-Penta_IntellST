fcb01fc8bb89d8c17a8c2494d8139ae8d7197a04
Ion98 <guideaion98@gmail.com>
2020-06-16 20:57:11 +0300

    Add IdentifiedCase

---
+++ intellst-api/src/Controller/IdentifiedCaseController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\IdentifiedCaseDTO;
+use App\Services\IdentifiedCaseHandler;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Serializer\ValidationErrorSerializer;
+
+class IdentifiedCaseController extends AbstractController
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * @var IdentifiedCaseHandler
+     */
+    private $identifiedCaseHandler;
+
+    /**
+     * @var ValidationErrorSerializer
+     */
+    private $validationErrorSerializer;
+
+    public function __construct(
+        IdentifiedCaseHandler $identifiedCaseHandler,
+        SerializerInterface $serializer,
+        ValidationErrorSerializer $validationErrorSerializer
+    )
+    {
+        $this->serializer = $serializer;
+        $this->identifiedCaseHandler = $identifiedCaseHandler;
+        $this->validationErrorSerializer = $validationErrorSerializer;
+    }
+
+    /**
+     * @Route("/api/identified-case", name="add_identified_case", methods={"POST"})
+     */
+    public function addIdentifiedCase(Request $request): JsonResponse
+    {
+        $data = $request->getContent();
+
+        $addIdentifiedCaseDTO = $this->serializer->deserialize(
+            $data,
+            IdentifiedCaseDTO::class,
+            'json'
+        );
+
+        $errors = $this->identifiedCaseHandler->updateIdentifiedCase($addIdentifiedCaseDTO);
+        if ($errors->count()) {
+            return new JsonResponse(
+                [
+                    'code' => Response::HTTP_BAD_REQUEST,
+                    'message' => 'Bad Request',
+                    'errors' => $this->validationErrorSerializer->serialize($errors),
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        return new JsonResponse(['message' => 'Identified Case added successfully'], Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/api/identified-case", name="add_identified_case", methods={"GET"})
+     */
+    public function listIdentifiedCase(): JsonResponse
+    {
+        $list = $this->identifiedCaseHandler->getList();
+
+        return new JsonResponse($list);
+    }
+}
