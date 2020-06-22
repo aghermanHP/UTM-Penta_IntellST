ef857969c6d07e590e51171b9238bdbb24fc5271
aghermanHP <adrian.gherman1@gmail.com>
2020-06-22 15:29:59 +0300

    was resolved problem with service Registration

--- intellst-api/src/Controller/AuthController.php
+++ intellst-api/src/Controller/AuthController.php
@@ -3,32 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\Register;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AuthController extends AbstractController
 {
     /**
      * AuthController constructor.
      * @param Register $register
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $encoder
      */
 
-    public function __construct( Register $register, Request $request, UserPasswordEncoderInterface $encoder) {
+    public function __construct( Register $register ) {
         $this->register = $register;
-        $this->request = $request;
-        $this->encoder = $encoder;
     }
     /**
      * @Route("/register", methods={"POST"})
      */
     public function register():string
     {
-        $this->register->register( $this->request, $this->encoder );
+        $this->register->register();
     }
 
     /**
--- intellst-api/src/Service/Register.php
+++ intellst-api/src/Service/Register.php
@@ -3,21 +3,34 @@
 namespace App\Service;
 
 use App\Entity\User;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\ORM\getDoctrine;
+
 
 class Register
 {
-    public function register(Request $request, UserPasswordEncoderInterface $encoder) : string
+    /**
+     * Register constructor.
+     * @param RequestStack $request
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(RequestStack $request, UserPasswordEncoderInterface $encoder)
+    {
+        $this->request = $request;
+        $this->encoder = $encoder;
+    }
+
+    public function register() : string
     {
         $em = $this->getDoctrine()->getManager();
 
-        $username = $request->request->get('username');
-        $password = $request->request->get('password');
+        $username = $this->request->request->get('username');
+        $password = $this->request->request->get('password');
 
         $user = new User($username);
-        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setPassword($this->encoder->encodePassword($user, $password));
         $em->persist($user);
         $em->flush();
 
